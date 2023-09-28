23a20dea927a4dfc43530702cf8795f06805c666
Akshay-Mahagan <tech.akshayk@gmail.com>
2023-09-28 10:48:21 +0530

    This is core java hands on practice repository

---
+++ exception_handling/ExceptionH1.java
@@ -0,0 +1,33 @@
+package exception_handling;
+
+import java.util.Scanner;
+
+/*Q.Write a Java program that throws an exception and catch it using a try-catch block.*/
+public class ExceptionH1 {
+
+    public static int division(int dividend, int divisor) {
+        if (divisor == 0) {
+            throw new ArithmeticException("Can not divide a number by zero!");
+        }
+        return dividend / divisor;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter dividend:");
+        int dividend = sc.nextInt();
+
+        System.out.println("enter divisor :");
+        int divisor = sc.nextInt();
+
+        //risky code
+        try {
+            int result = division(dividend, divisor);
+            System.out.println("result is: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error " + e.getMessage());
+        }
+    }
+
+}
